103547eb07492268a4545cb32f5d27db6b6b962e
ko Hyunsu <movie3342@gmail.com>
2022-02-21 17:21:48 +0900

    recoil selector : which state transforming??

--- toDo/src/components/ToDoList.tsx
+++ toDo/src/components/ToDoList.tsx
@@ -1,17 +1,30 @@
-import { toDoState } from 'atoms';
-import { useRecoilValue } from 'recoil';
-import CreateToDo from './CreateToDO';
-import ToDo from './ToDo';
+import { toDoSelector, toDoState } from "atoms";
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDO";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>오늘의 할 일</h1>
       <CreateToDo />
+      <h2>ToDo</h2>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <h2>Doing</h2>
+
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <h2>DONE</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
