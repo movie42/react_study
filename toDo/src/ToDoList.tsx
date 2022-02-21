cd6de885c0b1f7857d95305a9cb7d9923f5ce445
ko Hyunsu <movie3342@gmail.com>
2022-02-21 14:42:03 +0900

    use react-hook-form : useForm ()- register, watch

--- toDo/src/ToDoList.tsx
+++ toDo/src/ToDoList.tsx
@@ -1,35 +1,42 @@
-import React, { FormEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 function ToDoList() {
-  const [toDo, setToDo] = useState('');
-  const [toDoError, setToDoError] = useState('');
-  const onChange = (event: FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (toDo.length < 10) {
-      return setToDoError('길이가 10글자 이상이어야합니다.');
-    } else {
-      return setToDoError('');
-    }
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form>
         <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="할일 입력하기"
+          {...register('email')}
+          type="email"
+          placeholder="이메일을 입력하세요"
         />
-
+        <input
+          {...register('first_name')}
+          type="text"
+          placeholder="이름을 입력하세요."
+        />
+        <input
+          {...register('last_name')}
+          type="text"
+          placeholder="성을 입력하세요"
+        />
+        <input
+          {...register('user_name')}
+          type="text"
+          placeholder="사용하실 이름을 입력하세요."
+        />
+        <input
+          {...register('password')}
+          type="text"
+          placeholder="비밀번호를 입력하세요."
+        />
+        <input
+          {...register('password_confirm')}
+          type="text"
+          placeholder="앞에 입력한 비밀번호와 똑같은 비밀번호를 입력해주세요."
+        />
         <button>추가하기</button>
-        {toDoError !== '' ? toDoError : null}
       </form>
     </div>
   );
