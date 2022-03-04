5679d837c4710d21cb358fb250d4fc333dda137f
ko Hyunsu <movie3342@gmail.com>
2022-03-05 02:45:43 +0900

    animation project : variants

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -12,19 +12,56 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Circle = styled(motion.div)`
+  background-color: white;
+  place-self: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 40px;
+`;
+
+const boxVariants = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        transition={{ type: "spring", delay: 1, bounce: 0.45 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
