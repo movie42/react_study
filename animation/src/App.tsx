27336845e18a6367e82c66c8d324276c4e491fbe
ko Hyunsu <movie3342@gmail.com>
2022-03-05 14:49:10 +0900

    animation project : animate presence

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,10 +1,12 @@
-import { motion } from "framer-motion";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column-reverse;
   justify-content: center;
   align-items: center;
 `;
@@ -13,8 +15,9 @@ const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100px;
-  height: 100px;
+  width: 300px;
+  height: 200px;
+  margin-bottom: 2rem;
   background-color: #ffffff;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -22,52 +25,38 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.2, rotate: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(31, 255, 203)",
-    transition: { duration: 0.2 },
+  initail: {
+    opacity: 0,
+    scale: 0,
   },
-};
-
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  color: #fff;
-  path {
-    stroke: white;
-    stroke-width: 2;
-  }
-`;
-
-const svgVariant = {
-  start: { pathLength: 0, fill: "rgba(255,255,255, 0)" },
-  end: {
-    fill: "rgba(255,255,255, 1)",
-    pathLength: 1,
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 20,
   },
 };
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing((prev) => !prev);
   return (
     <Wrapper>
-      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <motion.path
-          variants={svgVariant}
-          initial="start"
-          animate="end"
-          transition={{
-            default: {
-              duration: 2,
-            },
-            fill: {
-              duration: 2,
-              delay: 1,
-            },
-          }}
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        />
-      </Svg>
+      <button onClick={toggleShowing}>클릭</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="initial"
+            animate="visible"
+            exit="leaving"
+          />
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
